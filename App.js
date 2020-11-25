3970da1e186ec783f3bd9989fbf329a8a06ec81b
Rémi Decouty <remi.decouty@ensiie.fr>
2020-11-25 17:02:46 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,119 +1,37 @@
-//React Native
-import React, { Component, Fragment } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Button } from 'react-native';
-//Third Party Modules
-import moment from "moment"; //format date and get curr date
-import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';//detect swipe gestures
-//Our Modules
-import {PrintPhoto, PrintText} from "./Photo.js";
-import Menu from "./Menu.js";
-
-const API = 'https://api.nasa.gov/planetary/apod?'
-const KEY = 'api_key=JsDc3pN9hPNPG3QD4Yg9gl2yt3EaJlGi4iLe0GgA'
-//nombre de requetes max de l'ordre de 10/20 sans api_key
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MenuScreen from './Menu';
+import LoginScreen from './Login';
+import SigninScreen from './Signin';
 
 
-class App extends Component {
-    state = {
-        date: moment(),
-        photo : ""
-    }
+const Stack = createStackNavigator();
 
-    formatDate = moment => {
-        let y = moment.year();
-        let m = moment.month() + 1; /* 0 indexed months */
-        let d = moment.date();
-        return `${y}-${m}-${d}`;
-    }
-
-    setDate(moment) {
-        this.setState({date: moment});
-        //date changed => update photo
-        this.getPhoto(this.formatDate(moment));
-    }
-
-    getDate() {
-        return moment(this.state.date); //cloning the currDate
-    }
-
-    //query with date as input
-    getPhoto = date => {
-        fetch(API + `date=${date}&` + KEY)
-            .then(response => response.json())
-            .then(photoData => this.setState({ photo: photoData }))
-            .catch((error) => {console.error(error);console.log("Probleme dans l'api call");} );
-    }
-
-    componentDidMount() {
-        this.setDate(moment());
-    }
-
-    //finger go right to left on the screen
-    swipeLeft() {
-        let currDate = this.getDate();
-        let newDate = moment.min(moment(), currDate.add(1,'days'));
-        this.setDate(newDate);
-    }
-
-    //finger go left to right on the screen
-    swipeRight() {
-        let currDate = this.getDate();
-        this.setDate(currDate.subtract(1,'days'));
-    }
-
-    mainApp() {
-        return(
-            <View style={{flex: 1,backgroundColor: '#D5CABD'}}>
-
-                <PrintPhoto inputPhoto={this.state.photo} />
-                <PrintText inputPhoto={this.state.photo} />
-
-                <View style={styles.buttonView}>
-                    <TouchableOpacity style = {styles.sideButtons} onPressIn={() => this.swipeRight()}>
-                        <Text style={{fontSize: 30}}>{'<'}</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style = {styles.sideButtons} onPressIn={() => this.swipeLeft()}>
-                        <Text style={{fontSize: 30}}>{'>'}</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        );
-    }
-
-    render() {
-        const config = {
-            velocityThreshold: 0.2,
-            directionalOffsetThreshold: 80
-        };
-        return (
-            <GestureRecognizer
-                onSwipeLeft={(state) => this.swipeLeft()}
-                onSwipeRight={(state) => this.swipeRight()}      // désactiver temporairement
-                style={{flex:1}}
-                config = {config}
-                >
-                {this.mainApp()}
-            </GestureRecognizer>
-        );
-    }
+export default class App extends React.Component {
+  
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{headerShown: false}}
+          />
+         <Stack.Screen
+            name="Signin"
+            component={SigninScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Menu"
+            component={MenuScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-    buttonView: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 50,
-        bottom: 0,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        zIndex: 2,
-    },
-    sideButtons: {
-        justifyContent: 'center',
-        height: 50,
-    },
-});
-
-export default App;
