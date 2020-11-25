3584215344d28fd338b1c3aa14dfd33e6e8f043b
Rémi Decouty <remi.decouty@ensiie.fr>
2020-11-25 09:14:05 +0000

    Ajout de la page d'inscription

---
+++ Signin.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default class Signin extends React.Component {
+  state={
+    username:"",
+    password:""
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>NASA App</Text>
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Nom d'utilisateur" 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({username:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Mot de passe" 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+        <TouchableOpacity style={styles.signinBtn}>
+          <Text style={styles.signinText}>Inscription</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.signinTextMargin}>  
+          <Text style={styles.signinText}>Retour à la page de connexion</Text>
+        </TouchableOpacity>
+  
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  signinBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  signinText:{
+    color:"white"
+  },
+  signinTextMargin:{
+    marginTop:15
+  }
+});
