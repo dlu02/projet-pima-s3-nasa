f307d92c62dd6e944a12f74e2b6cdebfc4b282d5
Damien Lu <ludamien2@gmail.com>
2020-12-09 12:47:59 +0100

    upload galerie, design à revoir

--- Galerie.js
+++ Galerie.js
@@ -1,110 +1,157 @@
-//React Native
-import React, { Component, Fragment } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Button, ToastAndroid, Image, TouchableHighlight } from 'react-native';
-//Third Party Modules
-import moment from "moment"; //format date and get curr date
-import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';//detect swipe gestures
-//Our Modules
-import {PrintPhoto} from "./PhotoGallery.js";
-import { ScrollView } from 'react-native-gesture-handler';
-import ic_like from './img/like.png';
+// Example of Grid Image Gallery in React Native
+// https://aboutreact.com/grid-image-gallery/
 
+// import React in our code
+import React, {useState, useEffect} from 'react';
 
-const API = 'https://api.nasa.gov/planetary/apod?'
-const KEY = 'api_key=JsDc3pN9hPNPG3QD4Yg9gl2yt3EaJlGi4iLe0GgA'
-//nombre de requetes max de l'ordre de 10/20 sans api_key
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from 'react-native';
 
-class Galerie extends React.Component {
-    state = {
-        date: moment(),
-        photo : ""
-    }
-    
-    formatDate = moment => {
-        let y = moment.year();
-        let m = moment.month() + 1; /* 0 indexed months */
-        let d = moment.date();
-        return `${y}-${m}-${d}`;
-    }
+//import FastImage
+import FastImage from 'react-native-fast-image';
 
-    setDate(moment) {
-        this.setState({date: moment});
-        //date changed => update photo
-        this.getPhoto(this.formatDate(moment));
-    }
+const App = () => {
+  const [imageuri, setImageuri] = useState('');
+  const [
+    modalVisibleStatus, setModalVisibleStatus
+  ] = useState(false);
+  const [dataSource, setDataSource] = useState([]);
 
-    getDate() {
-        return moment(this.state.date); //cloning the currDate
-    }
+  const createArray = () => {
+    var items = [];
+    for (let i =0 ; i<30 ; i++){
+      items.push({id: i, src: 'https://perso.dlu02.ovh/pima_S3/gallery.php?count=' + i })
+    };
+    return items
+  }
 
-    //query with date as input
-    getPhoto = date => {
-        fetch(API + `date=${date}&` + KEY)
-            .then(response => response.json())
-            .then(photoData => this.setState({ photo: photoData }))
-            .catch((error) => {console.error(error);console.log("Probleme dans l'api call");} );
-    }
+  useEffect(() => {
+    let items = createArray();
+    setDataSource(items);
+  }, []);
 
-    componentDidMount() {
-        this.setDate(moment());
-    }
-    mainApp() {
-        return(
-            <View style={{flex: 1,backgroundColor: '#D5CABD'}}>
+  const showModalFunction = (visible, imageURL) => {
+    //handler to handle the click on image of Grid
+    //and close button on modal
+    setImageuri(imageURL);
+    setModalVisibleStatus(visible);
+  };
 
-                <PrintPhoto inputPhoto={this.state.photo} />
-            </View>
-        );
-    }
-
-    render() {
-        const config = {
-            velocityThreshold: 0.2,
-            directionalOffsetThreshold: 80
-        };
-        return (
-            <GestureRecognizer
-                onSwipeLeft={(state) => this.swipeLeft()}
-                onSwipeRight={(state) => this.swipeRight()}      // désactiver temporairement
-                style={{flex:1}}
-                config = {config}
-                >
-                {this.mainApp()}
-            </GestureRecognizer>
-        );
-    }
-}
-
+  return (
+    <SafeAreaView style={styles.container}>
+      {modalVisibleStatus ? (
+        <Modal
+          transparent={false}
+          animationType={'fade'}
+          visible={modalVisibleStatus}
+          onRequestClose={() => {
+            showModalFunction(!modalVisibleStatus, '');
+          }}>
+          <View style={styles.modelStyle}>
+            <FastImage
+              style={styles.fullImageStyle}
+              source={{uri: imageuri}}
+              resizeMode={
+                FastImage.resizeMode.contain
+              }
+            />
+            <TouchableOpacity
+              activeOpacity={0.5}
+              style={styles.closeButtonStyle}
+              onPress={() => {
+                showModalFunction(!modalVisibleStatus, '');
+              }}>
+              <FastImage
+                source={{
+                  uri:
+                    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/close.png',
+                }}
+                style={{width: 35, height: 35}}
+              />
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      ) : (
+        <View style={styles.container}>
+          <Text style={styles.titleStyle}>
+            Grid Image Gallery in React Native
+          </Text>
+          <FlatList
+            data={dataSource}
+            renderItem={({item}) => (
+              <View style={styles.imageContainerStyle}>
+                <TouchableOpacity
+                  key={item.id}
+                  style={{flex: 1}}
+                  onPress={() => {
+                    showModalFunction(true, item.src);
+                  }}>
+                  <FastImage
+                    style={styles.imageStyle}
+                    source={{
+                      uri: item.src,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+            //Setting the number of column
+            numColumns={3}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
+export default App;
 
 const styles = StyleSheet.create({
-    buttonView: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 50,
-        bottom: 0,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        zIndex: 2,
-    },
-
-    buttonLike: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: -10,
-        bottom: 0,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        zIndex: 3,
-    },
-
-    sideButtons: {
-        justifyContent: 'center',
-        height: 50,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  titleStyle: {
+    padding: 16,
+    fontSize: 20,
+    color: 'white',
+    backgroundColor: 'green',
+  },
+  imageContainerStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 1,
+  },
+  imageStyle: {
+    height: 120,
+    width: '100%',
+  },
+  fullImageStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '98%',
+    resizeMode: 'contain',
+  },
+  modelStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  },
+  closeButtonStyle: {
+    width: 25,
+    height: 25,
+    top: 50,
+    right: 20,
+    position: 'absolute',
+  },
 });
-
-export default Galerie;
